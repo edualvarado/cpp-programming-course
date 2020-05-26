c7eb47177f79d36c70de13ffb9cde7801a839164
edualvarado <alvaradopinero.eduardo@gmail.com>
2020-05-26 12:27:19 +0200

    Update Section 10

--- Section-10-Characters-and-Strings/Section10/LetterPyramid/main.cpp
+++ Section-10-Characters-and-Strings/Section10/LetterPyramid/main.cpp
@@ -12,32 +12,30 @@ int main(){
     int length_input = input.length();
     int counter {0};
 
-    // Move though the word
-    for (char c: input) {
-
-        // Print spaces
+    do{
+        // Print spaces at the beginning
         int num_spaces = length_input - counter;
         while (num_spaces > 0) {
             cout << " ";
             num_spaces--;
         }
-
-        // Print previous letters to the char we are in the "for" loop
+        
+        // Print before the middle
         for (int i{0}; i < counter; i++) {
             cout << input[i];
         }
-
-        // Print char of "for"
-        cout << c;
-
-        // Print after 
+        
+        // Print middle of the line
+        cout << input[counter];
+        
+        // Print backwards after the middle
         for (int i{counter-1}; i >= 0; i--) {
             cout << input[i];
         }
 
-        cout << endl; // Don't forget the end line
+        cout << endl;         
         counter++;
-    }
-
+    } while(counter < length_input); 
+    
     return 0;
 }
